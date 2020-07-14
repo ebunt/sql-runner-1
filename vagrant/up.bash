84de032b5bc767daac5165f458b29032ed98dde1
jbeemster <joshua.a.beemster@gmail.com>
2020-07-14 14:03:11 +0200

    Update Vagrant installation - WIP

--- vagrant/up.bash
+++ vagrant/up.bash
@@ -7,35 +7,29 @@ bashrc=/home/vagrant/.bashrc
 echo "========================================"
 echo "INSTALLING PERU AND ANSIBLE DEPENDENCIES"
 echo "----------------------------------------"
-apt-get update
-apt-get install -y language-pack-en git unzip libyaml-dev python3-pip python-yaml python-paramiko python-jinja2
+sudo apt-get update
+sudo apt-get install -y language-pack-en git unzip libyaml-dev python3-pip python-yaml python-paramiko python-jinja2
+sudo apt install resolvconf
 
 echo "==============="
 echo "INSTALLING PERU"
 echo "---------------"
 sudo pip3 install peru
 
-echo "=================="
-echo "INSTALLING ANSIBLE"
-echo "------------------"
-apt-get update
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install -y ansible
-
 echo "======================================="
-echo "CLONING PLAYBOOKS WITH PERU"
+echo "CLONING ANSIBLE AND PLAYBOOKS WITH PERU"
 echo "---------------------------------------"
 cd ${vagrant_dir} && peru sync -v
 echo "... done"
 
+env_setup=${vagrant_dir}/ansible/hacking/env-setup
 hosts=${vagrant_dir}/ansible.hosts
 
 echo "==================="
 echo "CONFIGURING ANSIBLE"
 echo "-------------------"
 touch ${bashrc}
+echo "source ${env_setup}" >> ${bashrc}
 echo "export ANSIBLE_HOSTS=${hosts}" >> ${bashrc}
 echo "... done"
 
@@ -44,8 +38,9 @@ echo "RUNNING PLAYBOOKS WITH ANSIBLE*"
 echo "* no output while each playbook is running"
 echo "------------------------------------------"
 while read pb; do
-    su - -c "ansible-playbook ${vagrant_dir}/${pb} --connection=local --inventory-file=${hosts}" vagrant
+    su - -c "source ${env_setup} && ${vagrant_dir}/ansible/bin/ansible-playbook ${vagrant_dir}/${pb} --connection=local --inventory-file=${hosts}" vagrant
 done <${vagrant_dir}/up.playbooks
+source /etc/profile
 
 guidance=${vagrant_dir}/up.guidance
 
